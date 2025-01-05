1b39a27254a4f35d702963727e5b171950ba0000
LuccasAmbrozio <fluccas780@gmail.com>
2025-01-05 12:36:45 -0300

    Update acessando-obj.js

--- Objetos Alura/acessando-obj.js
+++ Objetos Alura/acessando-obj.js
@@ -1,13 +1,14 @@
 const aluno = {
-nome: 'Tiquinho Suarez',
-escola: 'Botafogo',
-nota: 'Libertadores'
+nome: 'Henry',
+escola: 'Lumiemre',
+nota: '8.5'
 }
 
 function infoEstudante (objAluno, infoAluno) {
     return objAluno[infoAluno]
 }
+//resumidamente, os colchetes transformam as chaves em strings que podem ser chamadas
 
 console.log(infoEstudante(aluno, 'escola'))
 console.log(infoEstudante(aluno, 'nome'))
-//resumidamente, os colchetes transformam as chaves em strings que podem ser chamadas
+//Retorna o resultado da chave
